4b93f7b04e0df6b1baa1d18b13891c087f5688fa
Haye Boonstra <haye1998@hotmail.com>
2026-03-05 13:50:28 +0100

    added windows virtual environment and fixed mujoco issues.

--- bicycle_test.py
+++ bicycle_test.py
@@ -1,5 +1,6 @@
 import mujoco
 import mujoco.viewer
+import time
 
 model = mujoco.MjModel.from_xml_path("bicycle.xml")
 data = mujoco.MjData(model)
@@ -9,3 +10,4 @@ viewer = mujoco.viewer.launch_passive(model, data)
 while True:
     mujoco.mj_step(model, data)
     viewer.sync()
+    time.sleep(0.01)
