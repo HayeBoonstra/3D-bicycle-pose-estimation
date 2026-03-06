17ca6d0f07410bee6f93bd9b8863cf0a3329cbe2
Haye Boonstra <haye1998@hotmail.com>
2026-03-06 09:19:21 +0100

    added world

--- bicycle_test.py
+++ bicycle_test.py
@@ -1,7 +1,8 @@
 import mujoco
 import mujoco.viewer
+import time
 
-model = mujoco.MjModel.from_xml_path("bicycle.xml")
+model = mujoco.MjModel.from_xml_path("world.xml")
 data = mujoco.MjData(model)
 
 viewer = mujoco.viewer.launch_passive(model, data)
@@ -9,3 +10,4 @@ viewer = mujoco.viewer.launch_passive(model, data)
 while True:
     mujoco.mj_step(model, data)
     viewer.sync()
+    time.sleep(0.01)
